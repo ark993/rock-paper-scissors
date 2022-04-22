1a4d22766a67751b3f57fbc71a2d062dc5ce626a
ark993 <renegade.spartan93@gmail.com>
2022-04-22 23:18:44 +0400

    created function for playRound

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
+let playerSelection = "rock";
 let computerSelection = computerPlay();
-let playerSelection = "Rock";
-
 
 function computerPlay() {
     let options = ["Rock", "Paper", "Scissors"];
@@ -9,13 +8,53 @@ function computerPlay() {
     return decision;
 }
 
-function playRound(playerSelection, computerPlay()) {
-    if(playerSelection == "Rock" && computerSelection == "Scisss") {
-        console.log("You win, Rock beats Scissors")
-    }
-    else com
-    console.log("Testing");
-    }
+function playRound(playerSelection, computerSelection) {
+    
+    switch(playerSelection) {
+        case "rock":
+            switch(computerSelection) {
+                case "Rock":
+                    console.log("nobody wins, both y'all picked rock");
+                    break;
+                case "Scissors": 
+                    console.log("U win, rock beat scissors");
+                    break;
+                case "Paper":
+                    console.log("U LOSE, paper beats rock!");
+                    break;
+            }
+        break;
+
+        case "paper":
+            switch(computerSelection) {
+                case "Rock":
+                    console.log("U win, paper beats rock!");
+                    break;
+                case "Scissors": 
+                    console.log("U lose, scissor beats paper");
+                    break;
+                case "Paper":
+                    console.log("nobody wins, both picked paper");
+                    break;
+            }
+        break;
+
+        case "scissors":
+            switch(computerSelection) {
+                case "Rock":
+                    console.log("U lose, rock beats scissors");
+                    break;
+                case "Scissors":
+                    console.log("Nobody wins, both picked scissors")
+                    break;
+                case "Paper":
+                    console.log("U win, scissors beat paper!")
+                    break;
+            }
+        break;
+        }
+       computerSelection = computerPlay();
+       }
 
 
 
